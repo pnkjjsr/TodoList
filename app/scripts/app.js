72192b430a8eb1a0edea4f657a142fb237b4dd97
pnkjjsr <pnkj_jsr@yahoo.co.in>
2016-01-19 18:20:17 +0530

    few more changes.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,9 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.sortable',
+    'LocalStorageModule'
   ])
   .config(function ($routeProvider) {
     $routeProvider
