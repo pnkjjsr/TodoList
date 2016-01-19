81cc3db8c4cbb840b61b9ab4da3533fdb777f0ac
pnkjjsr <pnkj_jsr@yahoo.co.in>
2016-01-19 17:20:19 +0530

    Yeoman angular server first project. For learning.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('yeomanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4'
+    ];
+    
+    $scope.addTodo = function() {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+    };
+    
+    $scope.removeTodo = function(index) {
+        $scope.todos.splice(index, 1);
+    };
+  });
